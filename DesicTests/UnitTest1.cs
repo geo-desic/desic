0905eed90b2b048b3d39b91d05008d574feff4a8
geo-desic <62725093+geo-desic@users.noreply.github.com>
2020-09-26 16:14:41 -0700

    Add project files.
    Signed-off-by: geo-desic <62725093+geo-desic@users.noreply.github.com>

---
+++ DesicTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DesicTests
+{
+    public class UnitTest1
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+        //    Assert.Equal(4, 4);
+        //}
+    }
+}
