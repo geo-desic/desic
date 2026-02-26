2af29062c158c6058f397a0c06fc566e3621550f
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-02-26 13:58:27 -0800

    corrected ValidationException reference

--- src/Application/Common/ValidationHelpers.cs
+++ src/Application/Common/ValidationHelpers.cs
@@ -1,10 +1,10 @@
-﻿using FluentValidation;
+﻿using Desic.Application.Common.Exceptions;
 
 namespace Desic.Application.Common;
 
 internal static class ValidationHelpers
 {
-    internal static void GuardAgainstInvalid<T>(this IValidator<T> validator, T instance)
+    internal static void GuardAgainstInvalid<T>(this FluentValidation.IValidator<T> validator, T instance)
     {
         var validationResult = validator.Validate(instance);
         if (!validationResult.IsValid)
