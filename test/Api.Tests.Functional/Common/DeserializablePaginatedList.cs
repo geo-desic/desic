1295657df3db18b2cc0ed58c0380bcfa3ccf5cde
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-03-01 15:18:06 -0800

    added a functional test for list entity types

---
+++ test/Api.Tests.Functional/Common/DeserializablePaginatedList.cs
@@ -0,0 +1,8 @@
+﻿namespace Desic.Api.Tests.Functional.Common;
+
+public class DeserializablePaginatedList<T>
+{
+    public int StartIndex { get; set; }
+    public int? TotalCount { get; set; }
+    public List<T> Items { get; set; } = [];
+}
---
+++ test/Api.Tests.Functional/Controllers/V1/EntityTypesControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using AwesomeAssertions;
+using Desic.Api.Tests.Functional.Common;
+using Desic.Application.EntityTypes;
+using Desic.Testing.Integration.Db;
+using Desic.Testing.Integration.Http;
+using Desic.Testing.Integration.WebApplication;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Desic.Api.Tests.Functional.Controllers.V1;
+
+public class EntityTypesControllerTests : IClassFixture<DbFixture>
+{
+    private readonly TestWebApplicationFactory<Program> _factory;
+    private readonly HttpClient _httpClient;
+
+    public EntityTypesControllerTests(DbFixture dbFixture)
+    {
+        _factory = new TestWebApplicationFactory<Program>(dbFixture.ConnectionStringApp);
+        _httpClient = _factory.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
+    }
+
+    [Fact]
+    public async Task List_All_Status200OkAndEntitiesReturned()
+    {
+        // arrange
+        var expected = ExpectedResponseContent(); // all entity types should exist by db seeding
+        var request = new FluentHttpRequest(HttpMethod.Get, $"/v1/entitytypes");
+
+        // act
+        var response = await _httpClient.SendAsyncAndReadResponseAsJson<DeserializablePaginatedList<EntityType>>(request);
+
+        // assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        response.Content.Should().NotBeNull();
+        response.Content.Should().BeEquivalentTo(expected, x => x.WithStrictOrdering());
+    }
+
+    private static DeserializablePaginatedList<EntityType> ExpectedResponseContent()
+    {
+        List<EntityType> items = [.. Domain.EntityTypes.SystemEntityTypes.AllAsEntities().Select(x => new EntityType { Key = x.Key, Name = x.Name }).OrderBy(x => x.Name)];
+        return new()
+        {
+            StartIndex = 0,
+            TotalCount = items.Count,
+            Items = items,
+        };
+    }
+}
