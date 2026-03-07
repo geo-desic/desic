cc40ad8002fd70843f09cee620e48440fd16522f
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-03-07 15:07:31 -0800

    converted all test projects to use microsoft testing platform (xunit.net v3)

---
+++ test/Infrastructure.Tests.Unit/DependencyInjectionTests.cs
@@ -0,0 +1,30 @@
+﻿using AwesomeAssertions;
+using Desic.Application.Common.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Desic.Infrastructure.Tests.Unit;
+
+public class DependencyInjectionTests
+{
+    public class DependencyInjectionTests001 : DependencyInjectionTests
+    {
+        [Fact]
+        public void AddInfrastructure_SpecifiedServiceCollection_RegistersExpectedServices()
+        {
+            // arrange
+            var serviceCollection = NewServiceCollection();
+
+            // act
+            serviceCollection.AddInfrastructure();
+
+            // assert
+            var serviceDescriptor = serviceCollection.SingleOrDefault(d => d.ServiceType == typeof(IApplicationDbContext));
+            serviceDescriptor.Should().NotBeNull();
+        }
+    }
+
+    private static IServiceCollection NewServiceCollection()
+    {
+        return new ServiceCollection();
+    }
+}
