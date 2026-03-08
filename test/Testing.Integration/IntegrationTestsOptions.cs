bae4604f6db9bae50896efd783e84ced0ca7ec4f
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-03-07 19:14:29 -0800

    IntegrationTestsOptions - replaced set with init

--- test/Testing.Integration/IntegrationTestsOptions.cs
+++ test/Testing.Integration/IntegrationTestsOptions.cs
@@ -2,38 +2,38 @@
 
 public sealed class IntegrationTestsOptions
 {
-    public IntegrationTestsDbProvidersOptions? DbProviders { get; set; }
-    public IntegrationTestsDatabaseOptions? Databases { get; set; }
-    public string? DbProvider { get; set; }
+    public IntegrationTestsDbProvidersOptions? DbProviders { get; init; }
+    public IntegrationTestsDatabaseOptions? Databases { get; init; }
+    public string? DbProvider { get; init; }
 }
 
 public sealed class IntegrationTestsDbProvidersOptions
 {
-    public IntegrationTestsDbProvidersSqlServerOptions? SqlServer { get; set; }
+    public IntegrationTestsDbProvidersSqlServerOptions? SqlServer { get; init; }
 }
 
 public sealed class IntegrationTestsDbProvidersSqlServerOptions
 {
-    public bool? UseContainer { get; set; }
-    public string? ContainerImage { get; set; }
+    public bool? UseContainer { get; init; }
+    public string? ContainerImage { get; init; }
 }
 
 public sealed class IntegrationTestsDatabaseOptions
 {
-    public IntegrationTestsDatabaseApplicationOptions? Application { get; set; }
+    public IntegrationTestsDatabaseApplicationOptions? Application { get; init; }
 }
 
 public sealed class IntegrationTestsDatabaseApplicationOptions
 {
-    public IntegrationTestsDatabaseApplicationSqlServerOptions? SqlServer { get; set; }
+    public IntegrationTestsDatabaseApplicationSqlServerOptions? SqlServer { get; init; }
 }
 
 public sealed class IntegrationTestsDatabaseApplicationSqlServerOptions
 {
-    public Dictionary<string, IntegrationTestsDatabaseApplicationSqlServerUsersOptions>? Users { get; set; }
+    public Dictionary<string, IntegrationTestsDatabaseApplicationSqlServerUsersOptions>? Users { get; init; }
 }
 
 public sealed class IntegrationTestsDatabaseApplicationSqlServerUsersOptions
 {
-    public string? Password { get; set; }
+    public string? Password { get; init; }
 }
