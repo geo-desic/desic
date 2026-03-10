2e9ca480a23c2b22441a13f1228edf313e46f3c3
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-03-09 21:42:00 -0700

    fixed a console log message

--- test/Testing.Integration/Db/TemplateDatabaseBasedOnConfig.cs
+++ test/Testing.Integration/Db/TemplateDatabaseBasedOnConfig.cs
@@ -23,17 +23,17 @@ public sealed class TemplateDatabaseBasedOnConfig : ITemplateDatabase
         else // sql server
         {
             var databaseInitializerOptions = _options?.Databases?.Application?.SqlServer ?? throw new InvalidOperationException($"Database initializer options for {DbProvider} is not configured");
-            if (_options?.DbProviders?.SqlServer?.UseContainer ?? false)
+            if (_options?.DbProviders?.SqlServer?.UseContainer ?? false) // container
             {
                 var apiUserPassword = databaseInitializerOptions.Users?.Api?.Password ?? throw new InvalidOperationException($"{nameof(DatabaseInitializerUsersOptions.Api)} user {nameof(DatabaseInitializerUserOptions.Password)} is not configured");
                 var image = _options?.DbProviders?.SqlServer?.ContainerImage ?? throw new InvalidOperationException("Container image for sql server is not configured");
                 Console.WriteLine($"Using database: {DbProvider} (container) {image}");
                 _database = new TemplateDatabaseSqlServerContainer(image: image, apiUserPassword: apiUserPassword);
             }
-            else // localdb
+            else // local
             {
                 var connectionStringInitialization = _options?.ConnectionStrings?.SqlServer ?? throw new InvalidOperationException($"Connection string {nameof(IntegrationTestsConnectionStringsOptions.SqlServer)} is not configured");
-                Console.WriteLine($"Using database: {DbProvider} (localdb)");
+                Console.WriteLine($"Using database: {DbProvider} (local)");
                 _database = new TemplateDatabaseSqlServerLocal(connectionStringInitialization: connectionStringInitialization, databaseDirectoryPath: databaseDirectoryPath, databaseInitializerOptions: databaseInitializerOptions);
             }
         }
