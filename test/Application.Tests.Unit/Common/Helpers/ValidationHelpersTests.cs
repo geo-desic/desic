f7d98c36d4354d78e7a93a9ae1652a226ce68f3a
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-03-13 21:34:00 -0700

    added ValidationHelpersTests

---
+++ test/Application.Tests.Unit/Common/Helpers/ValidationHelpersTests.cs
@@ -0,0 +1,58 @@
+﻿using AwesomeAssertions;
+using Desic.Application.Common.Helpers;
+using FluentValidation;
+
+namespace Desic.Application.Tests.Unit.Common.Helpers;
+
+public class ValidationHelpersTests
+{
+    public class ValidationHelpersTests001 : ValidationHelpersTests
+    {
+        [Fact]
+        public void InstanceIsValid_ValidModel_ReturnsTrueAndNullError()
+        {
+            // arrange
+            var instance = new TestModel { Name = "Name" }; // valid - name is non-null
+            var validator = new TestModelValidator();
+
+            // act
+            var result = ValidationHelpers.InstanceIsValid(validator: validator, instance: instance, out var error);
+
+            // assert
+            result.Should().BeTrue();
+            error.Should().BeNull();
+        }
+    }
+
+    public class ValidationHelpersTests002 : ValidationHelpersTests
+    {
+        [Fact]
+        public void InstanceIsValid_InvalidModel_ReturnsFalseAndNonNullError()
+        {
+            // arrange
+            var instance = new TestModel { Name = null }; // invalid - name is null
+            var validator = new TestModelValidator();
+
+            // act
+            var result = ValidationHelpers.InstanceIsValid(validator: validator, instance: instance, out var error);
+
+            // assert
+            result.Should().BeFalse();
+            error.Should().NotBeNull();
+        }
+    }
+
+    private class TestModel
+    {
+        public string? Name { get; set; }
+    }
+
+    private class TestModelValidator : AbstractValidator<TestModel>
+    {
+        public TestModelValidator()
+        {
+            RuleFor(u => u.Name)
+                .NotEmpty();
+        }
+    }
+}
