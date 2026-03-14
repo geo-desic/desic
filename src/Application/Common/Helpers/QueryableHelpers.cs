9641a93e27699eab1246decb5ad3a9c06ff66eb9
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-03-13 22:32:12 -0700

    renamed PaginatedListHelpers to QueryableHelpers

--- src/Application/Common/Helpers/QueryableHelpers.cs
+++ src/Application/Common/Helpers/QueryableHelpers.cs
@@ -2,7 +2,7 @@
 
 namespace Desic.Application.Common.Helpers;
 
-internal static class PaginatedListHelpers
+internal static class QueryableHelpers
 {
     private const int DefaultTakeCount = 100;
     public static async Task<PaginatedList<T>> ToPaginatedList<T>(this IQueryable<T> source, int startIndex = 0, int? takeCount = DefaultTakeCount, bool includeTotalCount = false, CancellationToken cancellationToken = default)
