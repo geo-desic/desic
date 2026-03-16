35cbfbf568249d0767c90643acfe008c4e516e5e
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-03-15 17:15:42 -0700

    added ListEntityTypesRequestHandlerTests

---
+++ test/Application.Tests.Unit/EntityTypes/List/ListEntityTypesRequestHandlerTests.cs
@@ -0,0 +1,71 @@
+﻿
+using AwesomeAssertions;
+using Desic.Application.Common;
+using Desic.Application.EntityTypes;
+using Desic.Application.EntityTypes.List;
+using Desic.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Desic.Application.Tests.Unit.EntityTypes.List;
+
+public class ListEntityTypesRequestHandlerTests : InMemoryEfCoreDependencyTests<TestApplicationDbContext>
+{
+    private readonly ILogger<ListEntityTypesRequestHandler> _logger = NullLogger<ListEntityTypesRequestHandler>.Instance;
+    private readonly Domain.EntityTypes.EntityType[] _seededEntityTypes;
+    private const int TotalCount = 150; // greater than ListEntityTypesRequestHandler.MaximumAllowedCount
+
+    public ListEntityTypesRequestHandlerTests() : base(o => new(o))
+    {
+        _seededEntityTypes = GetEntityTypes().ToArray();
+        DbContext.EntityTypes.AddRange(_seededEntityTypes);
+        DbContext.SaveChanges();
+    }
+
+    public class ListEntityTypesRequestHandlerTests001 : ListEntityTypesRequestHandlerTests
+    {
+        [Theory]
+        [InlineData(0, ListEntityTypesRequestHandler.MaximumAllowedCount, null, null)] // count == null, startIndex == null =====> maximum allowed ordered items returned
+        [InlineData(1, ListEntityTypesRequestHandler.MaximumAllowedCount, null, 1)]    // count == null, startIndex == 1    =====> maximum allowed ordered items returned except the first
+        [InlineData(0, 1, 1, null)]                                                    // count == 1   , startIndex == null =====> only first ordered item returned
+        [InlineData(0, 1, 1, -1)]                                                      // count == 1   , startIndex == -1   =====> only first ordered item returned (negative startIndex treated as 0)
+        [InlineData(1, 1, 1, 1)]                                                       // count == 1   , startIndex == 1    =====> only the second ordered item returned
+        public async Task Handle_SpecifiedRequestValues_ExpectedResults(int expectedMinIndex, int expectedCount, int? count, int? startIndex)
+        {
+            // arrange
+            var expectedStartIndex = startIndex ?? 0;
+            if (expectedStartIndex < 0) expectedStartIndex = 0;
+            var expected = new Result<ListEntityTypesResult>(new ListEntityTypesResult
+            {
+                Items = [.. ExpectedItems(minIndex: expectedMinIndex, count: expectedCount)],
+                StartIndex = expectedStartIndex,
+                TotalCount = ListEntityTypesRequestHandler.IncludeTotalCount ? TotalCount : null,
+            });
+            var handler = new ListEntityTypesRequestHandler(logger: _logger, dbContext: DbContext);
+            var request = new ListEntityTypesRequest { Count = count, StartIndex = startIndex };
+
+            // act
+            var result = await handler.Handle(request, cancellationToken: TestContext.Current.CancellationToken);
+
+            // assert
+            result.Should().BeOfType(expected.GetType());
+            result.Value.Should().BeOfType(expected.Value.GetType());
+            result.Value.Should().BeEquivalentTo(expected.Value);
+        }
+    }
+
+    private static IEnumerable<Domain.EntityTypes.EntityType> GetEntityTypes()
+    {
+        // purposely not ordered by name
+        for (var i = TotalCount; i > 0; --i)
+        {
+            var iString = $"{i}".PadLeft(3, '0');
+            yield return new Domain.EntityTypes.EntityType { Id = i.ToGuid(), Key = $"k{iString}", Name = $"Entity{iString}" };
+        }
+    }
+
+    private IEnumerable<EntityType> ExpectedItems(int minIndex, int count)
+    {
+        return _seededEntityTypes.OrderBy(x => x.Name).Skip(minIndex).Take(count).Select(x => new EntityType { Key = x.Key, Name = x.Name });
+    }
+}
---
+++ test/Application.Tests.Unit/TestApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Desic.Application.Common.Interfaces;
+using Desic.Domain.EntityTypes;
+using Desic.Domain.Iso3166Countries;
+using Desic.Domain.Tags;
+using Desic.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desic.Application.Tests.Unit;
+
+public class TestApplicationDbContext(DbContextOptions<TestApplicationDbContext> options) : DbContext(options), IApplicationDbContext
+{
+    public DbSet<EntityType> EntityTypes { get; set; }
+    public DbSet<Iso3166Country> Iso3166Countries { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<User> Users { get; set; }
+}
