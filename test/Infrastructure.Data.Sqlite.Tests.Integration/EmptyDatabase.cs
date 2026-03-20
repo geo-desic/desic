8ab7f82538fb729a67b178d04fc95bd6d5758c9d
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-03-20 03:58:18 -0700

    added new project: Desic.Infrastructure.Data.Sqlite.Tests.Integration

---
+++ test/Infrastructure.Data.Sqlite.Tests.Integration/EmptyDatabase.cs
@@ -0,0 +1,27 @@
+﻿using Desic.Testing.Integration;
+using Desic.Testing.Integration.Db;
+using Microsoft.Data.Sqlite;
+
+namespace Desic.Infrastructure.Data.Sqlite.Tests.Integration;
+
+public class EmptyDatabase : ITestDatabaseSqlite
+{
+    private readonly EmptyDatabaseSqlite _database = new(databaseDirectoryPath: Path.Combine(Path.GetTempPath(), Constants.TestsTempDirectoryName));
+
+    public string DatabaseDirectoryPath => _database.DatabaseDirectoryPath;
+    public string DatabaseFilePath => _database.DatabaseFilePath;
+    public string DatabaseFileName => _database.DatabaseFileName;
+    public string DatabaseName => _database.DatabaseName;
+    public SqliteConnection GetSqliteConnection() => _database.GetSqliteConnection();
+    public string GetConnectionString() => _database.GetConnectionString();
+
+    public async ValueTask InitializeAsync()
+    {
+        await _database.InitializeAsync();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _database.DisposeAsync();
+    }
+}
