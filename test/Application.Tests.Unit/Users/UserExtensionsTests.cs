a1776c2398f124ef8a02dbeeb3f5c31f898ce97a
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-03-24 20:50:31 -0700

    added application unit tests: UserExtensionsTests

---
+++ test/Application.Tests.Unit/Users/UserExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using AwesomeAssertions;
+using Desic.Application.Users;
+
+namespace Desic.Application.Tests.Unit.Users;
+
+public class UserExtensionsTests
+{
+    public class UserExtensionsTests001 : UserExtensionsTests
+    {
+        [Fact]
+        public void ToModel_AllMappedPropertiesSpecified_AllPropertiesMapped()
+        {
+            // arrange
+            var expected = new User
+            {
+                Username = "username-expected",
+            };
+            var source = new Domain.Users.User
+            {
+                Username = expected.Username,
+            };
+
+            // act
+            var result = UserExtensions.ToModel(source: source);
+
+            // assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
