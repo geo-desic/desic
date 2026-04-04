88fa6a852b4263165e9512b2cd568cdcd97ffba0
geo-desic <62725093+geo-desic@users.noreply.github.com>
2026-04-04 01:05:43 -0700

    fixed invalid enum JsonConverter

--- src/Application/EntityTypes/EntityTypesOrderingProperty.cs
+++ src/Application/EntityTypes/EntityTypesOrderingProperty.cs
@@ -1,11 +1,10 @@
-﻿using Desic.Application.Iso3166Countries;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace Desic.Application.EntityTypes;
 
 // entries alphabetized except potentially the first which should be the desired default value
 // recommended: each entry name exactly matches the corresponding property name in the domain entity
-[JsonConverter(typeof(JsonStringEnumConverter<Iso3166CountriesOrderingProperty>))]
+[JsonConverter(typeof(JsonStringEnumConverter<EntityTypesOrderingProperty>))]
 public enum EntityTypesOrderingProperty
 {
     Name, // default value
